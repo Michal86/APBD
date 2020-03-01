be6a6f5ace0ab1c89f0659dff7339b4203f8cbf9
s16712 <s16712@pjwstk.edu.pl>
2020-03-01 17:49:38 +0100

    Dodanie kodu mini scraper do e-maili.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            foreach (var a in args)
+            {
+                Console.WriteLine(a);
+            }
+
+            //var emails = await GetEmails(args[0]);
+            var emails = await GetEmails("https://www.pja.edu.pl/");
+
+            foreach (var email in emails)
+            {
+                Console.WriteLine(email);
+            }
+        }
+
+        static async Task<IList<string>> GetEmails(string url)
+        {
+            var httpClient = new HttpClient();
+            var listOfEmails = new List<string>();
+            var response = await httpClient.GetAsync(url);
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*",  RegexOptions.IgnoreCase);
+            //find items that matches with our pattern
+            MatchCollection emailMatches = emailRegex.Matches(responseBody);
+
+            foreach (var emailMatche in emailMatches)
+            {
+                listOfEmails.Add(emailMatche.ToString());
+            }
+
+            return listOfEmails;
         }
     }
 }
