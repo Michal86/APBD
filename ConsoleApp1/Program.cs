0ccc15105dc456e2fc2607ccbba9466aa69b4f26
Michał Radzewicz <radzewicz.michal@gmail.com>
2020-03-03 17:59:32 +0100

    Dodatkowe wymagania (#1)
    * Dodanie kodu mini scraper do e-maili.
    
    * Nowe punkty.
    
    * Update README.md
    
    * Update README.md
    
    * Dokończenie zadań 4 i 5

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,94 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //W sytuacji kiedy parametr 1 nie został przekazany, powinniśmy zgłosić błąd ArgumentNullException
+            if (args.Length > 0) {
+
+                foreach (var a in args)
+                {
+                    //W sytuacji kiedy przekazany parametr nie jest poprawnym adresem URL, powinniśmy zgłosić ArgumentException()
+                    if (await IsValidURL(a))
+                    {
+                        Console.WriteLine(a);
+                    }
+                    else
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+
+                var emails = await GetEmails(args[1]);
+                //var emails = await GetEmails("https://www.pja.edu.pl/");
+
+                foreach (var email in emails)
+                {
+                    Console.WriteLine(email);
+                }
+
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+            
+        }
+
+        static async Task<bool> IsValidURL(string URL)
+        {
+            string Pattern = @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
+            Regex Rgx = new Regex(Pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            return Rgx.IsMatch(URL);
+        }
+
+        static async Task<IList<string>> GetEmails(string url)
+        {
+            var httpClient = new HttpClient();
+            var listOfEmails = new List<string>();
+            HttpResponseMessage response = null;
+
+            //W sytuacji kiedy podczas pobierania wystąpił błąd - wyświetlamy informację "Błąd w czasie pobierania strony".
+            try
+            {
+                response = await httpClient.GetAsync(url);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nBłąd w czasie pobierania strony");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+            //Powinniśmy w poprawny sposób zwalniać zasoby (wykorzystanie metody Dispose()) związane z wykorzystaniem klasy HttpClient
+            string responseBody = await response.Content.ReadAsStringAsync();
+            response.Dispose();
+
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*",  RegexOptions.IgnoreCase);
+            //find items that matches with our pattern
+            MatchCollection emailMatches = emailRegex.Matches(responseBody);
+
+            if (emailMatches.Count == 0)
+            {
+                Console.WriteLine("Nie znaleziono adresów email");
+            }
+
+            foreach (var emailMatche in emailMatches)
+            {
+                if (!listOfEmails.Contains(emailMatche.ToString()))
+                {
+                    listOfEmails.Add(emailMatche.ToString());
+                }
+            }
+                
+            return listOfEmails;
         }
     }
 }
