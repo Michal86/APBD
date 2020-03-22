b7ad220bd8bbcaa3ecba98ba5a6b3ce2ea6bbb92
Michał Radzewicz <s16712@pjwstk.edu.pl>
2020-03-22 18:14:58 +0100

    Zadanie 3 - Dodanie kontrolera

---
+++ Cw3/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public string GetStudent()
+        {
+            return "Kowalski, Malewski, Test";
+        }
+    }
+}
