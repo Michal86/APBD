e846aecd5e5ffb89d13eea059b85e10766b1e60b
Michał Radzewicz <s16712@pjwstk.edu.pl>
2020-05-22 21:20:47 +0200

    upd

---
+++ Cw3/Controllers/EnrollmentsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [Route("api/enrollments")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        private const string ConnString = "Data Source=db-mssql;Initial Catalog=s16712;Integrated Security=True";
+
+        [HttpPost]
+        public void EnrollStudent()
+        {
+            using (var connection = new SqlConnection(ConnString))
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                connection.Open();
+
+                var tran = connection.BeginTransaction();
+
+                try
+                {
+
+                }
+                catch(SqlException ex)
+                {
+                    tran.Rollback();
+                  
+                }
+
+                var response = new Enrollment();
+                return CreatedAtAction(response);
+            }
+        }
+
+        private void CreatedAtAction(Enrollment response)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Cw3/Models/EnrollmentDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw3.Models
+{
+    public class EnrollmentDTO
+    {
+        [Required]
+        public string IndexNumber { get; internal set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+        public string Name { get; set; }       //studies -> "IT"
+    }
+}
